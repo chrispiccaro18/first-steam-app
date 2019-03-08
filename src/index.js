b928034864f1f811d2470bb7a0d73dfdfec6a3bd
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-08 14:29:24 -0800

    first fetch

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import loadUserDisplay from './user-display-component.js';
 import user from '../data/user.js';
 import './search-component.js';
 import { readIDFromHash } from './hash-query.js';
+import { makeURLFromID } from './url-for-api.js';
 
 // http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=A74835CC435349DFEECF607F8BAA5834&steamids=76561198087595919
 
-loadUserDisplay(user);
+window.addEventListener('hashchange', () => {
+    const id = readIDFromHash(window.location.hash.slice(1));
+    const idURL = makeURLFromID(id);
 
-console.log(readIDFromHash(window.location.hash.slice(1)));
+    fetch(idURL)
+        .then(response => response.json())
+        .then(body => console.log(body));
+});
